e61229944443117d6a281efd5558b6a2ff363047
dwrench07 <dheerajreddynannuri1709@gmail.com>
2022-12-25 20:09:55 -0500

    Sorting commit

---
+++ sorting/bubblesort.py
@@ -0,0 +1,7 @@
+# this is a bubble sort
+def bubblesort():
+    print("Bubble sort")
+
+
+if __name__ == "__main__":
+    bubblesort()
---
+++ sorting/selectionsort.py
@@ -0,0 +1,6 @@
+def selection_sort():
+    print("Bubble sort")
+
+
+if __name__ == "__main__":
+    selection_sort()
